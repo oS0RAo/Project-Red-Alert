59a3a29f8470a1819199528144acdf20ded9460f
BANK047 <bankchoojongkol@gmail.com>
2026-03-08 16:39:01 +0700

    add red-alertdb / Example

--- BackEnd/src/controllers/index.ts
+++ BackEnd/src/controllers/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import { prisma } from "../lib/prisma"
+ 
 const app = express();
 const port = process.env.PORT || 2077;
 
@@ -8,6 +9,11 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('From API Port: 2077')
 });
+
+app.post('/register/users', (req, res) => {
+    const newUser = req.body
+    
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost${port}`);
